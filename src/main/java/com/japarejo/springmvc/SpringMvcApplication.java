8f78d0df3b3a162770b42b06a93378e364e43619
allumno <allumno@allumno-VirtualBox>
2019-12-12 12:01:24 +0100

    Examples of projections

--- src/main/java/com/japarejo/springmvc/SpringMvcApplication.java
+++ src/main/java/com/japarejo/springmvc/SpringMvcApplication.java
@@ -3,9 +3,12 @@ package com.japarejo.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableHypermediaSupport(type = { HypermediaType.HAL })
 public class SpringMvcApplication {
 
 	public static void main(String[] args) {
